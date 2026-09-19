48e1ee4e544e2d7221a6ebb875000b94fd2b1e03
fazlulkarimyousuf <faizmiah52@gmail.com>
2026-09-19 16:25:10 +0600

    my full projet is done ...

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
-
-import './App.css'
+import "./App.css";
+import Banner from "./components/Banner";
+import ExploreTheTechnologies from "./components/ExploreTheTechnologies";
+import Footer from "./components/Footer";
+import Footer2 from "./components/Footer2";
+import Navbar from "./components/Navbar";
 
 function App() {
-
   return (
     <>
-    
+      <Navbar></Navbar>
+      <Banner></Banner>
+      <ExploreTheTechnologies />
+      <Footer></Footer>
+      <Footer2></Footer2>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
+// Nameer-B14-A05-DevStack
